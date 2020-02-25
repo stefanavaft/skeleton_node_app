cd45c7fd925881fa12c1ea7a9c7ef6996f665d44
Eunice K <kontor.eunice@gmail.com>
2020-02-25 19:40:55 +0000

    attempt at selecting and manipulating data from the api result - failed

--- public/main.js
+++ public/main.js
@@ -1,6 +1,46 @@
 const request = require('request');
 
-function getPokemon(pokes){
-const element = document.getElementById ('mainData');
-element.style.color = newColor;
+
+
+
+{/* <div id="mainData">      
+<% countData["results"].forEach(function(Pokemon) { %>
+  <li><%= Pokemon["name"] %> - <%= Pokemon["url"] %></li>
+<% }) %>
+</div> */}
+
+const pokeContent = document.getElementById("mainData");
+
+function dropDown(){
+    const selectElement = document.getElementById("searchPoke");
+    console.log(document);
+
+    pokeContent.forEach(function(search){
+        let opt = document.createElement ('option');
+        opt.value = search.url;
+        opt.innerHTML = search.name;
+        selectElement.appendChild(opt);
+    });
+
+    selectElement.onchange = function(e){
+        const search = e.target.value;
+        const selected_poke_element = document.getElementById("selected_poke");
+        selected_poke_element.textContent = `Selected Pokemon is: ${search}`;
+        console.log(search);
+        fetchPokemon(search);
+    }
 }
+
+function fetchPokemon(search){
+    let url = `https://pokeapi.co/api/v2/pokemon/?offset=`;
+    request (url, function(err, response, body){
+        if(err) console.log('Error: ', err);
+        const pokemonContent = JSON.parse(body)
+
+        const get_poke = document.getElementById("name");
+        get_poke.textContent = `${pokemonContent.main.name}`
+        console.log(JSON.parse(body)); 
+    });
+}
+}
+
